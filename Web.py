23884241cecc53d036883d5f74ce2a92e36e4d07
Kfir <kfir.zusman@gmail.com>
2023-12-03 20:52:17 +0200

    added html elems

--- Web.py
+++ Web.py
@@ -4,6 +4,7 @@ import functions
 todos = functions.get_todos()
 
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -13,8 +14,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my Todo App")
-st.write("This app is here to help you increase productivity!")
+st.write("This app is here to help you increase <b>productivity!</b>",unsafe_allow_html=True)
 
+st.text_input("", placeholder="Add new Todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=index)
